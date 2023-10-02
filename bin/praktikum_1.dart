044c215df7105bbc4629b4673e7d6fd475737d64
Syaroful <syaroful10403@gmail.com>
2023-10-02 12:55:42 +0700

    deleting unnecessery file

--- bin/praktikum_1.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  final List<dynamic> list = [null, null, null, null, null];
-  print(list.length);
-  list[1] = "Muhamad Syaroful Anam";
-  list[2] = 2141720108;
-
-  print(list);
-}
